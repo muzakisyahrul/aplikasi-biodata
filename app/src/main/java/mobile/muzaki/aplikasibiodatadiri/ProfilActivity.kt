0e9d1715696a292eac5f606c5395a31f2fe0ea23
Muzaki <muzakisyahrul100@gmail.com>
2021-11-15 22:18:50 +0700

    edit profil dan dial telpon

--- app/src/main/java/mobile/muzaki/aplikasibiodatadiri/ProfilActivity.kt
+++ app/src/main/java/mobile/muzaki/aplikasibiodatadiri/ProfilActivity.kt
@@ -1,13 +1,19 @@
 package mobile.muzaki.aplikasibiodatadiri
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_profil.*
 class ProfilActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profil)
         ambilData()
+        btnEditName.setOnClickListener { navigasiKeEditProfil() }
+        btnCall.setOnClickListener { dialPhoneNumber(txtTelp.text.toString()) }
     }
 
     private fun ambilData(){
@@ -26,5 +32,42 @@ class ProfilActivity : AppCompatActivity() {
         txtAddress.text = alamat
 
     }
+
+    companion object {
+        val REQUEST_CODE  = 100
+    }
+
+    //fungsi untuk berpindah ke EditProfilActivity
+    private fun navigasiKeEditProfil() {
+        val intent = Intent(this, EditProfilActivity::class.java)
+
+        val namaUser = txtName.text.toString()
+        intent.putExtra("nama", namaUser)
+
+        startActivityForResult(intent, REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE){
+            if (resultCode == Activity.RESULT_OK) {
+                val result = data?.getStringExtra("nama")
+                txtName.text = result
+            }else{
+                Toast.makeText(this, "Edit failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun dialPhoneNumber(phoneNumber: String) {
+        val intent = Intent(Intent.ACTION_DIAL).apply {
+            data = Uri.parse("tel:$phoneNumber")
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+
+
 
 }
